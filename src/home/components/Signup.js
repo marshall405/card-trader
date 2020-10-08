3f569f96d15a782bd3475ea7d480ad440e7f4b22
marshall405 <marshall.slemp@gmail.com>
2020-10-08 17:03:26 -0500

    removes unused containers

--- src/home/components/Signup.js
+++ src/home/components/Signup.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import Container from '@material-ui/core/Container';
 
 const userURL = `${process.env.REACT_APP_API}/users`
 
